2479c735c1aad2a6125acf37fc40e64871fc4174
LucianoSeravalli <seravalliluciano@gmail.com>
2026-07-14 15:26:18 -0600

    Primer Commit Inicial avance 1.1

---
+++ gotocr/src/main/java/gotocr/gotocr/GotocrApplication.java
@@ -0,0 +1,13 @@
+package gotocr.gotocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotocrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotocrApplication.class, args);
+	}
+
+}
---
+++ gotocr/src/test/java/gotocr/gotocr/GotocrApplicationTests.java
@@ -0,0 +1,13 @@
+package gotocr.gotocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotocrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
